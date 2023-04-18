bf186f8377ec6e4f4b221d9b4b1889c7f00c84cc
Borwoll <github.com/Borwoll>
2023-04-18 14:30:31 +0300

    correction: the MenuItem model was described incorrectly

--- tree_menu/models.py
+++ tree_menu/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from django.urls import reverse
 
 
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class MenuItem(models.Model):
     title = models.CharField(max_length=100)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='items')
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
     url = models.CharField(max_length=100, blank=True)
     named_url = models.CharField(max_length=100, blank=True)
@@ -18,10 +26,3 @@ class MenuItem(models.Model):
             return self.url
         else:
             return '#'
-
-
-class Menu(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
